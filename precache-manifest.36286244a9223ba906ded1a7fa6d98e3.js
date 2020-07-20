0c1348b84bbca2edd4df906dd2cfa7256e1ed2ee
vincent caldwell <vcaldwel8@yahoo.com>
2020-07-20 15:11:43 -0400

    Updates

---
+++ precache-manifest.36286244a9223ba906ded1a7fa6d98e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c949d283b6556d1f75d59189015faaba",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "d97804a8632bc957d373",
+    "url": "/ToDoList/static/js/2.c22529fb.chunk.js"
+  },
+  {
+    "revision": "8020f58154580abedfaa",
+    "url": "/ToDoList/static/js/main.346edebc.chunk.js"
+  },
+  {
+    "revision": "171eec0cb52e30580581",
+    "url": "/ToDoList/static/js/runtime-main.f0d9ca9e.js"
+  }
+]);
